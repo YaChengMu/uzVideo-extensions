6b7282e0f93f0668294b9fe4a1d39b290e41465c
YaChengMu <114004363@qq.com>
2025-08-13 17:50:22 +0800

    init

--- vod/js/Pan_tgs_api.js
+++ vod/js/Pan_tgs_api.js
@@ -1,6 +1,6 @@
 //@name:[盘] TG纯搜
-//@version:5
-//@webSite:123资源@zyfb123&天翼日更@tianyirigeng&天翼臻影@tyysypzypd&云巢@peccxinpd&夸克UC@ucquark&夸克电影@alyp_4K_Movies&夸克剧集@alyp_TV&夸克动漫@alyp_Animation
+//@version:6
+//@webSite:123资源@zyfb123&天翼日更@tianyirigeng&天翼臻影@tyysypzypd&云巢@peccxinpd&夸克UC@ucquark&夸克电影@Q_dianying&夸克剧集@Q_dianshiju&夸克动漫@Q_dongman
 //@env:TG搜API地址##https://tgsou.252035.xyz
 //@remark:🍃豆儿出品，不属精品！免代理纯搜索，格式 频道名称@频道id|搜索数量&频道名称@频道id，支持自定义每频道搜索数量，默认3个
 //@order: A18
@@ -49,7 +49,7 @@ import { cheerio, Crypto, Encrypt, JSONbig } from '../../core/core/uz3lib.js'
 // ignore
 
 const appConfig = {
-    _webSite: '123资源@zyfb123&天翼日更@tianyirigeng&天翼臻影@tyysypzypd&云巢@peccxinpd&夸克UC@ucquark&夸克电影@alyp_4K_Movies&夸克剧集@alyp_TV&夸克动漫@alyp_Animation',
+    _webSite: '123资源@zyfb123&天翼日更@tianyirigeng&天翼臻影@tyysypzypd&云巢@peccxinpd&夸克UC@ucquark&夸克电影@Q_dianying&夸克剧集@Q_dianshiju&夸克动漫@Q_dongman',
     /**
      * 网站主页，uz 调用每个函数前都会进行赋值操作
      * 如果不想被改变 请自定义一个变量
@@ -92,10 +92,7 @@ const CLOUD_PROVIDERS = {
         name: 'UC',
         domains: ['drive.uc.cn']
     },
-    ali: {
-        name: '阿里',
-        domains: ['alipan.com', 'aliyundrive.com']
-    },
+
     pan123: {
         name: '123',
         domains: ['123pan.com', '123pan.cn', '123684.com', '123912.com', '123865.com']
